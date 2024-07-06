b743756b9b0db013e629c3c9b9124181faff647c
imyourlio <lrautvuori@gmail.com>
2024-07-06 14:23:23 +0700

    Update Check game.lua

--- Check game.lua
+++ Check game.lua
@@ -1,5 +1,3 @@
-if game.PlaceId == 7694862453 then -- Pay to win
-    return loadstring(game:HttpGet('https://raw.githubusercontent.com/imyourlio/Xameitz/main/Pay%20to%20Win.lua'))()
- elseif game.PlaceId == 15597903831 then -- Boys vs girls
-    return loadstring(game:HttpGet('https://raw.githubusercontent.com/imyourlio/Xameitz/main/Girls%20vs%20Boys.lua'))()
- end
+if game.PlaceId == 7694862453 then -- Loyal sea
+    return loadstring(game:HttpGet('https://raw.githubusercontent.com/imyourlio/Xameitz/main/Loyal%20Seas'))()
+end
