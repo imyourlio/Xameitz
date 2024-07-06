4f42b36aa8560195ec054c9802a0ce18fffbc2d0
imyourlio <lrautvuori@gmail.com>
2024-07-06 14:24:55 +0700

    Update Check game.lua

--- Check game.lua
+++ Check game.lua
@@ -1,3 +1,3 @@
-if game.PlaceId == 7694862453 then -- Loyal sea
+if game.PlaceId == 17545130379 then -- Loyal sea
     return loadstring(game:HttpGet('https://raw.githubusercontent.com/imyourlio/Xameitz/main/Loyal%20Seas'))()
 end
